1e1049739e70e5a06f084c4420be04a5f4c76244
AhshanHabib26 <habib.net27@gmail.com>
2022-04-30 00:12:27 +0600

    MiddleWare And Initial Server Setup

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 5000
+
+//MiddleWare
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) =>{
+    res.send('I Am Ahshan Habib')
+})
+
+
+app.listen(port)
+
