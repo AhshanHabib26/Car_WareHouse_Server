cbf9055559c0b25e83ca206ca562af4b27e6c2ea
AhshanHabib26 <habib.net27@gmail.com>
2022-05-07 21:17:12 +0600

    Delete API Updated

--- index.js
+++ index.js
@@ -50,6 +50,13 @@ async function carHouse() {
       res.send(result);
     });
 
+    app.delete("/additem/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userItemsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.get("/item", async (req, res) => {
       const query = {};
       const item = carHouseCollection.find(query);
